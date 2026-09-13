b8a1a1bda4ac20c0a2e8a8bd3e49de7d9f9ee709
Protozoa7 <159817051+Protozoa7@users.noreply.github.com>
2026-09-13 09:16:33 -0600

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,12 @@
 // Firebase's web config is not a private secret. Firestore Security Rules protect your data.
 // Paste the config object from Firebase Console > Project Settings > Your apps > Web app.
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyChPHTC4w_uc368DblYaG-3YBi3dVKnihA",
+  authDomain: "protocollection-3898f.firebaseapp.com",
+  projectId: "protocollection-3898f",
+  storageBucket: "protocollection-3898f.firebasestorage.app",
+  messagingSenderId: "392168038411",
+  appId: "1:392168038411:web:2d47b332e383202c3ba70e"
 }
 
 export const firebaseConfigured =
